b85cdf5849482250a6b1c1d1d9e69aa300ef1d36
hugodmorais <hugodanielmorais@gmail.com>
2020-05-22 00:21:16 +0100

    Finish script, crontab

--- WebsiteBlocker/website_blocker.py
+++ WebsiteBlocker/website_blocker.py
@@ -9,6 +9,20 @@ website_list=["www.abola.pt", "abola.pt", "www.record.pt"]
 while True:
   if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
     print("Working hours...")
+    with open(hosts, 'r+') as file:
+      content=file.read()
+      for website in website_list:
+        if website in content:
+          pass
+        else:
+          file.write(redirect+" "+ website+"\n")
   else:
+    with open(hosts,'r+') as file:
+      content=file.readlines()
+      file.seek(0)
+      for line in content:
+        if not any(website in line for website in website_list):
+          file.write(line)
+      file.truncate()
     print("Fun hours...")
   time.sleep(5)
